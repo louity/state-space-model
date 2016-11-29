f8979dcbcf1cc248a9f3be998caa4dd6a2d6e39b
Louis THIRY <louis.thiry@outlook.fr>
2016-11-29 18:24:07 +0100

    first commit with base files

---
+++ src/main.py
@@ -0,0 +1,7 @@
+# coding: utf8
+""" packages
+    Le package panda definit des dataframe avec plein de fonctions pratiques
+    voir http://pandas.pydata.org/
+"""
+import numpy as np
+import pandas as pd
