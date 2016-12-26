6394f912aab13d4ce969624e61c9206eed840085
Louis THIRY <louis.thiry@outlook.fr>
2016-12-26 15:43:04 +0100

    separate tests for linear anx extended kalman filters

--- src/kalmanTest.py
+++ src/kalmanTest.py
@@ -47,19 +47,56 @@ class TestStateSpaceModel(unittest.TestCase):
                         self.assertEqual(x.size, ssm.state_dim)
                         self.assertEqual(y.size, ssm.output_dim)
 
-    def test_kalman_methods(self):
+    def test_linear_kalman_methods(self):
         """
-            teste les méthodes kalman_filtering et kalman_smoothing dans plusieurs cas
+            teste les méthodes kalman_filtering et kalman_smoothing dans le cas linéaire
         """
-        for i, (is_f_linear, is_g_linear) in enumerate(zip([True, False, True, False], [True, True, False, False])):
+        for i, (state_dim, output_dim, input_dim) in enumerate(zip([1, 4, 3], [1, 3, 4], [0, 0, 0])):
+            # tester différentes tailles
+            for j, T in enumerate([1, 10]):
+                default_message = 'Parameters : T = ' + str(T) + '. state_dim = ' + str(state_dim) + '.  output_dim = ' + str(output_dim) + '. input_dim = ' + str(input_dim)
+                ssm = StateSpaceModel(is_f_linear=True, is_g_linear=True, state_dim=state_dim, output_dim=output_dim, input_dim=input_dim)
+                ssm.draw_sample(T=T)
+                ssm.kalman_smoothing(is_extended=False)
+                self.assertEqual(len(getattr(ssm, 'filtered_state_means')), T)
+                self.assertEqual(len(getattr(ssm, 'filtered_state_covariance')), T)
+                self.assertEqual(len(getattr(ssm, 'smoothed_state_means')), T)
+                self.assertEqual(len(getattr(ssm, 'smoothed_state_covariance')), T)
+
+                # verifier que les moyennes et covariances estimmées ont les bonnes dimensions
+                for i in range(0,T):
+                    xFilter0 = ssm.filtered_state_means[i][0]
+                    xFilter1 = ssm.filtered_state_means[i][1]
+                    PFilter0 = ssm.filtered_state_covariance[i][0]
+                    PFilter1 = ssm.filtered_state_covariance[i][1]
+
+                    xSmooth = ssm.smoothed_state_means[i]
+                    PSmooth = ssm.smoothed_state_covariance[T - 1 - i]
+
+                    self.assertEqual(xFilter0.size, ssm.state_dim, 'mean vector must have state_dim dimension')
+                    self.assertEqual(xFilter1.size, ssm.state_dim, 'mean vector must have state_dim dimension')
+                    self.assertEqual(PFilter0.shape, (ssm.state_dim, ssm.state_dim), 'cov matrix must have state_dim dimension')
+                    self.assertEqual(PFilter1.shape, (ssm.state_dim, ssm.state_dim), 'cov matrix must have state_dim dimension')
+                    # check that matrices are definite positive
+                    self.assertTrue(is_pos_def(PFilter0), 'filtered covariance matrix must be positive definite')
+                    self.assertTrue(is_pos_def(PFilter1), 'filtered covariance matrix must be positive definite')
+
+                    self.assertEqual(xSmooth.size, ssm.state_dim, 'mean vector must have state_dim dimension')
+                    self.assertEqual(PSmooth.shape, (ssm.state_dim, ssm.state_dim), 'cov matrix must have state_dim dimension')
+                    self.assertTrue(is_pos_def(PSmooth), default_message + '. Smoothed covariance P_{T-' + str(i) +'} matrix must be positive definite')
+
+    def test_extended_kalman_methods(self):
+        """
+            teste les méthodes kalman_filtering et kalman_smoothing dans le cas ou f ou g sont non linéaires
+        """
+        for i, (is_f_linear, is_g_linear) in enumerate(zip([False, True, False], [True, False, False])):
             # tester différentes dimensions pour les espaces
             for j, (state_dim, output_dim, input_dim) in enumerate(zip([1, 4, 3], [1, 3, 4], [0, 0, 0])):
                 # tester différentes tailles
                 for k, T in enumerate([1, 10]):
                     ssm = StateSpaceModel(is_f_linear=is_f_linear, is_g_linear=is_g_linear, state_dim=state_dim, output_dim=output_dim, input_dim=input_dim)
                     ssm.draw_sample(T=T)
-                    is_extended = not is_f_linear or not is_g_linear
-                    ssm.kalman_smoothing(is_extended=is_extended)# la methode kalman_smoothing appelle la méthode_kalman filtering
+                    ssm.kalman_smoothing(is_extended=True)
                     self.assertEqual(len(getattr(ssm, 'filtered_state_means')), T)
                     self.assertEqual(len(getattr(ssm, 'filtered_state_covariance')), T)
                     self.assertEqual(len(getattr(ssm, 'smoothed_state_means')), T)
