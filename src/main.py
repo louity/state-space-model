1214316aead4a49fc7f21c37c2d3b776799d8a2b
Louis THIRY <louis.thiry@outlook.fr>
2017-01-02 19:36:46 +0100

    run simu in main script

--- src/main.py
+++ src/main.py
@@ -5,3 +5,16 @@
 """
 import numpy as np
 import pandas as pd
+import testFunctions as testF
+import Simu as Simu
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import random
+
+X1=Simu.X1 
+Y1=Simu.Y1 
+U1=Simu.U1
+X1_noisy=Simu.X1_noisy
+Y1_noisy=Simu.Y1_noisy
+
